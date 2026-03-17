f1bcc1efdf4f9c897bff0db15a399793ea9873b5
MiraM243 <s33629@pjwstk.edu.pl>
2026-03-17 16:28:34 +0100

    Dodanie znajdowania najwiekszej liczby w tablicy

--- apbd-cw1-kod-s33162/src/StatisticsHelper.java
+++ apbd-cw1-kod-s33162/src/StatisticsHelper.java
@@ -16,4 +16,14 @@ public class StatisticsHelper {
         }
         return min;
     }
+
+    public static int maxInt(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
