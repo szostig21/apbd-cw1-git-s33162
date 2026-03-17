547ada14c43520993fbcb6cd15411164c57772b9
MiraM243 <s33629@pjwstk.edu.pl>
2026-03-17 16:39:50 +0100

    Dodanie main

--- apbd-cw1-kod-s33162/src/Main.java
+++ apbd-cw1-kod-s33162/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("essa");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj liczbe elementow w tablicy: ");
+        int size = Integer.parseInt(sc.nextLine());
+
+        int[] array = new int[size];
+
+        System.out.println("Podaj " + size + " liczb calkowitych: ");
+        for (int i = 0; i < size; i++) {
+            array[i] = Integer.parseInt(sc.nextLine());
+        }
+
+        int avg = StatisticsHelper.average(array);
+        int min = StatisticsHelper.minInt(array);
+        int max = StatisticsHelper.maxInt(array);
+
+        System.out.println("Srednia: " + avg + "\nNajmniejsza liczba: " + min
+                + "\nNajwieksza liczba: " + max);
     }
 }
