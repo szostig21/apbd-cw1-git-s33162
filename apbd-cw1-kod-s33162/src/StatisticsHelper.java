6e55700c00906d2abbad2b6d239e6a5697b8aeb7
MiraM243 <s33629@pjwstk.edu.pl>
2026-03-17 16:26:23 +0100

    Dodanie metody average

--- apbd-cw1-kod-s33162/src/StatisticsHelper.java
+++ apbd-cw1-kod-s33162/src/StatisticsHelper.java
@@ -1,3 +1,9 @@
 public class StatisticsHelper {
-
+    public static double average(double[] arr) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum / arr.length;
+    }
 }
